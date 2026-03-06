e61650c204bd96a9ae03deeb30c9923ce6bdb257
takoyaki-chan <nkmrksk@msn.com>
2026-03-06 23:00:38 +0900

    進行中

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -17,7 +17,7 @@ export async function onRequest(context) {
         status: 302,
         headers: {
           "Location": url.origin + "/",
-          ""Set - Cookie": "wm_auth = ok; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=86400"
+          "Set-Cookie": "wm_auth=ok; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=86400"
         }
       });
     }
